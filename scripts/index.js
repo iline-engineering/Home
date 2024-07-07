86c6e4aad31b5f67d30b53522758143c2b5807bf
kramzer22 <markdavidpanganiban1218@hotmail.com>
2024-07-07 17:38:20 +0800

    add navigation

--- scripts/index.js
+++ scripts/index.js
@@ -1,93 +1,43 @@
-let currentSlide = 0;
-
-const hamburgerMenu = document.getElementById("hamburger-menu");
+const navMenu = document.querySelector(".nav-menu");
 const header = document.querySelector("header");
-const navLinks = document.getElementById("nav-links");
+const navLinks = document.querySelector(".nav-links");
 
 document.addEventListener("DOMContentLoaded", function () {
   // scroll event listener
-  document.addEventListener("scroll", function () {
+  document.addEventListener("scroll", () => {
     // set header opacity when scrolling
     if (window.scrollY > 1) {
       header.style.opacity = 0.9;
     } else {
       header.style.opacity = 1;
     }
-
-    // if (navLinks.classList.contains("open")) {
-    //   navLinks.classList.remove("open");
-    // }
   });
 
-  hamburgerMenu.addEventListener("click", () => {
-    navLinks.classList.toggle("open");
+  navMenu.addEventListener("click", () => {
+    console.log("click");
+    navLinks.style.display = "flex";
   });
 
-  // Full screen nav indicator animation effect
-
-  const marker = document.querySelector(".marker");
-  const navItems = document.querySelectorAll(".nav-item");
-  let selectedNav = navItems[0];
-
-  navItems.forEach((item) => {
-    item.addEventListener("click", (e) => {
-      console.log("wawawee");
-      selectedNav = item;
-      setMarker(e.target, marker);
-    });
-
-    item.addEventListener("mouseover", (e) => {
-      setMarker(e.target, marker);
-    });
-
-    item.addEventListener("mouseout", () => {
-      setMarker(selectedNav, marker);
-    });
+  navLinks.addEventListener("click", () => {
+    navLinks.style.display = "none";
   });
 
-  setMarker(selectedNav, marker);
+  document.querySelectorAll(".same-page-nav").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-  // Animation load or display on screen
+      document.querySelectorAll(".nav-container a").forEach((link) => {
+        link.classList.remove("active");
+      });
 
-  const animatedElements = document.querySelectorAll(
-    ".appear, .appearBottom, .appearLeft, .appearRight"
-  );
+      this.classList.add("active");
 
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add("in-view");
-        observer.unobserve(entry.target); // Stop observing after the animation is triggered
-      }
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      targetElement.scrollIntoView({
+        behavior: "smooth",
+      });
     });
   });
-
-  animatedElements.forEach((element) => observer.observe(element));
 });
-
-// Function for nav indicator animation
-const setMarker = (e, marker) => {
-  marker.style.left = e.offsetLeft + "px";
-  marker.style.width = e.offsetWidth + "px";
-};
-
-// Set an interval to move the slide every 4 seconds (4000 milliseconds)
-const slideInterval = setInterval(() => {
-  moveSlide(1); // Move to the next slide
-}, 4000);
-
-function moveSlide(n) {
-  const items = document.querySelectorAll(".carousel-item");
-  const totalSlides = items.length;
-
-  const width = window.innerWidth;
-
-  if (width < 1025) {
-    // Calculate the new slide index
-    currentSlide = (currentSlide + n + totalSlides) % totalSlides;
-
-    // Move the carousel
-    const carouselInner = document.querySelector(".carousel-inner");
-    carouselInner.style.transform = `translateX(-${currentSlide * 100}%)`;
-  }
-}
